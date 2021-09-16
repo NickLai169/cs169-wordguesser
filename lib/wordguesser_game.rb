8dfa58bfb6e7839432867ec905e50f098fcc9fe2
Nick Lai <nickplai@berkeley.edu>
2021-09-16 08:47:32 +0000

    Finished part 2

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -10,6 +10,68 @@ class WordGuesserGame
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @word_with_guesses = "-"*word.length
+    return self
+  end
+  
+  def guess(letter)
+    def is_letter?(lookAhead)
+      return lookAhead.match?(/[[:lower:]]/)
+    end
+    
+    if letter == nil
+      raise ArgumentError.new("Guess cannot be nil") 
+    elsif letter == ""
+        raise ArgumentError.new("Guess must be nonempty")
+    elsif !letter.match?(/[[:alpha:]]/)
+      raise ArgumentError.new("Guess must be letter")
+    end
+    
+    if !is_letter?(letter) or @guesses.include?(letter)\
+      or @wrong_guesses.include?(letter)
+      return FALSE
+    end
+    
+    if @word.include?(letter)
+      @guesses += letter
+      
+      for i in 0...@word.length
+        if @word[i] == letter
+          @word_with_guesses[i] = letter
+        end
+      end
+    else
+      @wrong_guesses += letter
+    end
+    return TRUE
+  end
+  
+  def check_win_or_lose()
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif @guesses.length == @word.length
+      return :win
+    else
+      return :play
+    end
+  end
+  
+  def word()
+    return @word
+  end
+  
+  def guesses()
+    return @guesses
+  end
+  
+  def wrong_guesses()
+    return @wrong_guesses
+  end
+  
+  def word_with_guesses()
+    return @word_with_guesses
   end
 
   # You can test it by installing irb via $ gem install irb
