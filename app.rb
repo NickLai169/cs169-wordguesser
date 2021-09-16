b7be139220fd4831fc3d4e95ea30fa83943b71f1
Nick Lai <nickplai@berkeley.edu>
2021-09-16 22:09:07 +0000

    maybe finished behaviour for /guess

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if letter == nil
+      flash[:message] = "Invalid guess."
+    elsif @game.guesses.include?(letter) or \
+        @game.wrong_guesses.include?(letter)
+      flash[:message] = "You have already used that letter."
+    else
+      @game.guess(letter)
+    end
     redirect '/show'
   end
   
