0b7862daaf61a93a7f6d4edb8faaa36370243659
Nick Lai <nickplai@berkeley.edu>
2021-09-16 22:37:36 +0000

    checks for non-letter characters

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if letter == nil
+    if letter == nil or !letter.match?(/[[:alpha:]]/)
       flash[:message] = "Invalid guess."
     elsif @game.guesses.include?(letter) or \
         @game.wrong_guesses.include?(letter)
