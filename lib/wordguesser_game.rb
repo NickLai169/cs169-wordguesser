da5fe9eeca09788ec829edf949ce5f4d16cea856
Nick Lai <nickplai@berkeley.edu>
2021-09-16 22:19:34 +0000

    better implementation for game_win

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -52,7 +52,8 @@ class WordGuesserGame
   def check_win_or_lose()
     if @wrong_guesses.length >= 7
       return :lose
-    elsif @guesses.length == @word.length
+#     elsif @guesses.length == @word.length
+    elsif !@word_with_guesses.include?("-")
       return :win
     else
       return :play
